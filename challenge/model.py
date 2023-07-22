45c68d1037c99e8da4631ae6490fad40b919e40f
jpdattas <jpdattas@uc.cl>
2023-07-22 09:41:36 -0400

    fix model.py

--- challenge/model.py
+++ challenge/model.py
@@ -1,40 +1,11 @@
 import pandas as pd
+import numpy as np
 from typing import Tuple, Union, List
 from sklearn.linear_model import LogisticRegression
+from datetime import datetime
 
 class DelayModel:
-
-    def __init__(
-        self
-    ):
-        self._model = LogisticRegression(class_weight='balanced') # Model should be saved in this attribute.
-
-    def preprocess(
-        self,
-        data: pd.DataFrame,
-        target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
-        """
-        Prepare raw data for training or predict.
-
-        Args:
-            data (pd.DataFrame): raw data.
-            target_column (str, optional): if set, the target is returned.
-
-        Returns:
-            Tuple[pd.DataFrame, pd.DataFrame]: features and target.
-            or
-            pd.DataFrame: features.
-        """
-        features = pd.concat([
-            pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
-            pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'), 
-            pd.get_dummies(data['MES'], prefix = 'MES'),
-            ], 
-            axis = 1
-        )
-
-        top_10_features = [
+    TOP_10_FEATURES = [
             "OPERA_Latin American Wings", 
             "MES_7",
             "MES_10",
@@ -46,8 +17,29 @@ class DelayModel:
             "OPERA_Sky Airline",
             "OPERA_Copa Air"
         ]
-        
-        features = features[top_10_features]
+    THRESHOLD_IN_MINUTES = 15
+
+    def __init__(self):
+        self._model = LogisticRegression(class_weight='balanced')
+
+    @staticmethod
+    def _get_min_diff(row):
+        fecha_o = datetime.strptime(row['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+        fecha_i = datetime.strptime(row['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+        min_diff = ((fecha_o - fecha_i).total_seconds()) / 60
+        return min_diff
+
+    def preprocess(self, data: pd.DataFrame, target_column: str = None) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
+        data['min_diff'] = data.apply(self._get_min_diff, axis = 1)
+        data['delay'] = np.where(data['min_diff'] > self.THRESHOLD_IN_MINUTES, 1, 0)
+
+        features = pd.concat([
+            pd.get_dummies(data['OPERA'], prefix='OPERA'),
+            pd.get_dummies(data['TIPOVUELO'], prefix='TIPOVUELO'),
+            pd.get_dummies(data['MES'], prefix='MES')
+        ], axis=1)
+
+        features = features[self.TOP_10_FEATURES]
         
         if target_column:
             target = data[target_column]
@@ -55,32 +47,9 @@ class DelayModel:
         else:
             return features
 
-    def fit(
-        self,
-        features: pd.DataFrame,
-        target: pd.DataFrame
-    ) -> None:
-        """
-        Fit model with preprocessed data.
-
-        Args:
-            features (pd.DataFrame): preprocessed data.
-            target (pd.DataFrame): target.
-        """
+    def fit(self, features: pd.DataFrame, target: pd.DataFrame) -> None:
         self._model.fit(features, target)
         return
 
-    def predict(
-        self,
-        features: pd.DataFrame
-    ) -> List[int]:
-        """
-        Predict delays for new flights.
-
-        Args:
-            features (pd.DataFrame): preprocessed data.
-        
-        Returns:
-            (List[int]): predicted targets.
-        """
+    def predict(self, features: pd.DataFrame) -> List[int]:
         return self._model.predict(features)
